3f03a799d533a87b8b24a091135273d6d6dcac54
Aldion Lee <lxdavid.lee@gmail.com>
2022-07-17 14:20:35 +0900

    Add files for creating the database updater
    Also adds a pyproject.toml file for managing project configurations.

---
+++ updater/path_fix.py
@@ -0,0 +1,5 @@
+import os
+import sys
+
+ROOT = os.getcwd()
+sys.path.append(os.path.join(ROOT, "database"))
---
+++ updater/updater.py
@@ -0,0 +1,32 @@
+import sys
+import time
+
+import numpy as np
+import path_fix
+from db_urlconfig import get_db_url
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import WinningNumber
+
+
+class MyConnection:
+    def __init__(self):
+        db_rl = get_db_url()
+        self.session_maker = sessionmaker(bind=create_engine(db_rl))
+
+    def add_to_winning_numbers(self, scores):
+        scores = WinningNumber(number=scores[0], bonus=scores[1])
+        with self.session_maker() as session:
+            session.add(scores)
+
+            session.commit()
+
+
+conn = MyConnection()
+print("Conection Established")
+while True:
+    num1, num2 = np.random.randint(1, 36), np.random.randint(1, 36)
+    conn.add_to_winning_numbers((num1, num2))
+    print("Record added")
+    time.sleep(15)
